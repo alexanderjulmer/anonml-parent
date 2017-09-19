e56d0c648fe4499f6a17f849ad0a461d2f9f19f1
Mirco <ablx@posteo.de>
2017-09-19 19:56:49 +0200

    Download original file

--- src/main/java/ml/anon/web/controller/AppController.java
+++ src/main/java/ml/anon/web/controller/AppController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
+
 import okhttp3.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -41,152 +42,137 @@ import javax.servlet.http.HttpServletResponse;
 @Log
 public class AppController {
 
-  @Value("${documentmanagement.service.url}")
-  private String documentManagementUrl;
-  @Value("${rulebased.service.url}")
-  private String rulebasedUrl;
-  @Value("${machinelearning.service.url}")
-  private String machinelearningUrl;
+    @Value("${documentmanagement.service.url}")
+    private String documentManagementUrl;
+    @Value("${rulebased.service.url}")
+    private String rulebasedUrl;
+    @Value("${machinelearning.service.url}")
+    private String machinelearningUrl;
 
-  @Resource
-  private DocumentResource documentResource;
+    @Resource
+    private DocumentResource documentResource;
 
-  private RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate = new RestTemplate();
 
 
-  @RequestMapping(value = {"/", "/document/{id}"}, method = RequestMethod.GET)
-  public String index() {
-    System.out.println("index page accessed!");
-    return "forward:/index.html";
-  }
-
-  @RequestMapping(value = "/api/document/{id}", method = RequestMethod.GET)
-  public ResponseEntity<Document> loadDocument(@PathVariable("id") String id) {
-      System.out.println("Pre load document!");
-      return ResponseEntity.ok(documentResource.findById(id));
-  }
-
-  @PostMapping(value = "/api/update/anonymizations/{id}")
-  public ResponseEntity<Boolean> updateAnonymizations(@PathVariable String id,
-      @RequestBody List<Anonymization> anonymizations) {
-    try {
-
-      Document doc = documentResource.findById(id);
-      doc.setAnonymizations(anonymizations);
-      documentResource.update(id, doc);
-
-    } catch (Exception e) {
-      log.severe(e.getLocalizedMessage());
+    @RequestMapping(value = {"/", "/document/{id}"}, method = RequestMethod.GET)
+    public String index() {
+        System.out.println("index page accessed!");
+        return "forward:/index.html";
     }
 
-    return ResponseEntity.ok(true);
-  }
-
-  @GetMapping(value = "/api/save/{id}", produces = "application/zip")
-  public void saveEditedFile(@PathVariable String id, HttpServletResponse response)
-      throws IOException {
-
-    System.out.println("export-accessed!");
-
-    this.calculateFOne(id);
-    this.updateTrainingData(id);
-
-    URI url = URI.create(documentManagementUrl + "/document/" + id + "/export");
-
-    OkHttpClient client = new OkHttpClient();
-    Request req = new Request.Builder().url(HttpUrl.get(url)).build();
-    Response execute = client.newCall(req).execute();
-    ResponseBody body = execute.body();
-    execute.headers();
-    response.setContentType("application/zip");
-    response.addHeader("Content-Disposition", "attachment; filename=" + id + ".zip");
-    org.apache.commons.io.IOUtils
-        .copy(execute.body().source().inputStream(), response.getOutputStream());
-    response.getOutputStream().flush();
-  }
-
-  private boolean calculateFOne(String documentId){
-
-    return restTemplate.postForObject(URI.create(machinelearningUrl + "/ml/calculate/f/one/" + documentId), null,Boolean.class);
-  }
-
-  private boolean updateTrainingData(String documentId) {
-    return restTemplate.postForObject(
-        URI.create(machinelearningUrl + "/ml/update/training/data/" + documentId), null,
-        Boolean.class);
-  }
-
-  @GetMapping(value = "/api/labels")
-  public ResponseEntity<List<Label>> getAllLabels() {
-    return ResponseEntity.ok(Label.getAll());
-  }
-
-  @GetMapping(value = "/api/retrain")
-  public ResponseEntity<Boolean> retrainModel() {
-
-    ResponseEntity<Boolean> response = restTemplate
-        .getForEntity(URI.create(machinelearningUrl + "/ml/retrain/"), Boolean.class);
-
-    return ResponseEntity.ok(response.getBody());
-  }
-
-
-  @PostMapping("/api/upload")
-  public ResponseEntity<?> callDocumentManagement(@RequestParam(value = "file") MultipartFile file)
-      throws IOException {
-
-    byte[] bytes = file.getBytes();
-    String base64 = Base64Utils.encodeToString(bytes);
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.set(HttpHeaders.CONTENT_TYPE, "multipart/form-data");
-    MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
-    map.add("doc", base64);
-    map.add("title", file.getOriginalFilename());
-    HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(map, headers);
-
-    ResponseEntity<Document> exchange = restTemplate
-        .exchange(documentManagementUrl + "/document/import", HttpMethod.POST, entity,
-            Document.class);
-    log.info(String.valueOf(exchange));
-    ResponseEntity<Document> res = exchange;
-
-    Document body = res.getBody();
-
-    List<Anonymization> anonymizations = new ArrayList<>();
-    try {
-      anonymizations.addAll(this.applyRules(body));
-    } catch (Exception e) {
-      log.severe("RegEx Service not available");
-      log.severe(e.getLocalizedMessage());
+    @RequestMapping(value = "/api/document/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Document> loadDocument(@PathVariable("id") String id) {
+        System.out.println("Pre load document!");
+        return ResponseEntity.ok(documentResource.findById(id));
     }
-    try {
-      anonymizations.addAll(this.applyML(body));
-    } catch (Exception e) {
-      log.severe("ML Service not available");
-      log.severe(e.getLocalizedMessage());
+
+    @PostMapping(value = "/api/update/anonymizations/{id}")
+    public ResponseEntity<Boolean> updateAnonymizations(@PathVariable String id,
+                                                        @RequestBody List<Anonymization> anonymizations) {
+        try {
+
+            Document doc = documentResource.findById(id);
+            doc.setAnonymizations(anonymizations);
+            documentResource.update(id, doc);
+
+        } catch (Exception e) {
+            log.severe(e.getLocalizedMessage());
+        }
+
+        return ResponseEntity.ok(true);
     }
-    Document doc = documentResource.findById(body.getId());
-    doc.setAnonymizations(anonymizations);
-    body = documentResource.update(body.getId(), doc);
 
-    return ResponseEntity.ok(body);
+    @GetMapping(value = "/api/save/{id}", produces = "application/zip")
+    public void saveEditedFile(@PathVariable String id, HttpServletResponse response)
+            throws IOException {
+
+        System.out.println("export-accessed!");
+
+        this.calculateFOne(id);
+        this.updateTrainingData(id);
+
+        URI url = URI.create(documentManagementUrl + "/document/" + id + "/export");
+
+        OkHttpClient client = new OkHttpClient();
+        Request req = new Request.Builder().url(HttpUrl.get(url)).build();
+        Response execute = client.newCall(req).execute();
+        ResponseBody body = execute.body();
+        execute.headers();
+        response.setContentType("application/zip");
+        response.addHeader("Content-Disposition", "attachment; filename=" + id + ".zip");
+        org.apache.commons.io.IOUtils
+                .copy(execute.body().source().inputStream(), response.getOutputStream());
+        response.getOutputStream().flush();
+    }
+
+    private boolean calculateFOne(String documentId) {
+
+        return restTemplate.postForObject(URI.create(machinelearningUrl + "/ml/calculate/f/one/" + documentId), null, Boolean.class);
+    }
+
+    private boolean updateTrainingData(String documentId) {
+        return restTemplate.postForObject(
+                URI.create(machinelearningUrl + "/ml/update/training/data/" + documentId), null,
+                Boolean.class);
+    }
+
+    @GetMapping(value = "/api/labels")
+    public ResponseEntity<List<Label>> getAllLabels() {
+        return ResponseEntity.ok(Label.getAll());
+    }
+
+    @GetMapping(value = "/api/retrain")
+    public ResponseEntity<Boolean> retrainModel() {
+
+        ResponseEntity<Boolean> response = restTemplate
+                .getForEntity(URI.create(machinelearningUrl + "/ml/retrain/"), Boolean.class);
+
+        return ResponseEntity.ok(response.getBody());
+    }
 
 
-  }
+    @PostMapping("/api/upload")
+    public ResponseEntity<?> callDocumentManagement(@RequestParam(value = "file") MultipartFile file)
+            throws IOException {
 
-  private List<Anonymization> applyRules(Document document) {
-    return restTemplate.postForObject(
-        URI.create(rulebasedUrl + "/rules/annotate/" + document.getId()), null,
-        ArrayList.class);
+        byte[] bytes = file.getBytes();
+        Document body = documentResource.importDocument(file.getOriginalFilename(), bytes);
 
-  }
+        List<Anonymization> anonymizations = new ArrayList<>();
+        try {
+            anonymizations.addAll(this.applyRules(body));
+        } catch (Exception e) {
+            log.severe("RegEx Service not available");
+            log.severe(e.getLocalizedMessage());
+        }
+        try {
+            anonymizations.addAll(this.applyML(body));
+        } catch (Exception e) {
+            log.severe("ML Service not available");
+            log.severe(e.getLocalizedMessage());
+        }
+        Document doc = documentResource.findById(body.getId());
+        doc.setAnonymizations(anonymizations);
+        body = documentResource.update(body.getId(), doc);
 
-  private List<Anonymization> applyML(Document document) {
-    return restTemplate.postForObject(
-        URI.create(machinelearningUrl + "/ml/annotate/" + document.getId()), null, ArrayList.class);
+        return ResponseEntity.ok(body);
 
-  }
+
+    }
+
+    private List<Anonymization> applyRules(Document document) {
+        return restTemplate.postForObject(
+                URI.create(rulebasedUrl + "/rules/annotate/" + document.getId()), null,
+                ArrayList.class);
+
+    }
+
+    private List<Anonymization> applyML(Document document) {
+        return restTemplate.postForObject(
+                URI.create(machinelearningUrl + "/ml/annotate/" + document.getId()), null, ArrayList.class);
+
+    }
 
 
 }
